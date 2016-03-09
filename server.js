0fec64132f276c43661019571200642f18bf476e
GaMaur Landrum <gamaur.landrum@gmail.com>
2016-03-09 13:37:20 -0800

    i think i finally fixed it

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,6 +10,7 @@ var app = express();
 
 
  // uncomment after placing your favicon in /public
+ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
  app.use(logger('dev'));
  app.use(bodyParser.json());
  app.use(bodyParser.urlencoded({ extended: false }));
